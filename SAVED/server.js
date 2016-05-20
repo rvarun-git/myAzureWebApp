a2cbab1cc7d6782c11341d1a41bf622c07ad2e67
Russ Arun <rvarun@microsoft.com>
2016-05-19 19:15:45 -0700

    SAVING Old Stuff

---
+++ SAVED/server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Wow - can really publish chnage in VS Code to Azure Web site!</h1>');
+}).listen(process.env.PORT);
