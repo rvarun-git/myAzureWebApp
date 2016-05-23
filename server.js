f14b933e690252513be362423d28fc0fa951fffa
Russ Arun <rvarun@microsoft.com>
2016-05-23 13:57:43 -0700

    Back to HTTP

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var restify = require('restify');
+// var restify = require('restify');
 
-var server = restify.createServer();
-server.get("/api/messages", function(req, res) {
+// var server = restify.createServer();
+// server.get("/api/messages", function(req, res) {
     res.send("Wowie: Server started");
-});
-server.listen(process.env.PORT || 8080, function(){
-    console.log("%s listening on %s", server.name, server.url); 
-});
+// });
+// server.listen(process.env.PORT || 8080, function(){
+//     console.log("%s listening on %s", server.name, server.url); 
+// });
 
 
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log('Got request for ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('<h1>Wow - can really publish chnage in VS Code to Azure Web site!</h1><br>Also saving old stuff');
-// }).listen(process.env.PORT);
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Wow - can really publish chnage in VS Code to Azure Web site!</h1><br>Also saving old stuff');
+}).listen(process.env.PORT);
