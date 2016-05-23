18cda7f175dbb3adcc39dfa54e9d7b74291b3d99
Russ Arun <rvarun@microsoft.com>
2016-05-23 14:16:25 -0700

    HTTP Azure

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Wow - can really publish chnage in VS Code to Azure Web site!</h1><br>Also saving old stuff');
+    res.end('<h1>Wow - can really publish change in VS Code to Azure Web site!</h1><br>Also saving old stuff');
 }).listen(process.env.PORT); 
