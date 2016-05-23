d117365b7399623240578bbe0a601285ca2664b7
Russ Arun <rvarun@microsoft.com>
2016-05-23 14:46:39 -0700

    Back yet again to HTTP

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-var restify = require('restify');
+// CANNOT get Restify code to work :(
+    
+// var restify = require('restify');
 
-var server = restify.createServer();
-server.get("/api/messages", function(req, res) {
-   res.send("Wowie: Server started");
-});
-server.listen(process.env.PORT, function(){
-    console.log("%s listening on %s", server.name, server.url); 
-});
+// var server = restify.createServer();
+// server.get("/api/messages", function(req, res) {
+//    res.send("Wowie: Server started");
+// });
+// server.listen(process.env.PORT, function(){
+//     console.log("%s listening on %s", server.name, server.url); 
+// });
 
 
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log('Got request for ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('<h1>Wow - can really publish change in VS Code to Azure Web site!</h1><br>Also saving old stuff');
-// }).listen(process.env.PORT);
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log('Got request for ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Wow - can really publish change in VS Code to Azure Web site!</h1><br>Also saving old stuff');
+}).listen(process.env.PORT);
